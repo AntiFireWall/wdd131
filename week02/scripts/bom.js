89eee38256de62fda6ef7a2524e744f261de3747
BuildTools <unconfigured@null.spigotmc.org>
2024-07-13 16:38:33 +0400

    new files added

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,20 +1,47 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+let chaptersArray = getChapertList();// || [];
 
-button.addEventListener('click', function() {
+button.addEventListener('click', async function() {
     if (input.value.trim() !== ''){
-        li.textContent = input.value;
-        deleteButton.textContent = '❌';
-        li.append(deleteButton);
-        list.append(li);
+        displayList(input.value);
+        await chaptersArray.push(input.value);
+        setChapterList();
         input.value = '';
+        input.focus();
     };
 });
 
-deleteButton.addEventListener('click', function() {
-    list.removeChild(li);
-    input.focus();
-});
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+  });
+
+function displayList(item) {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = item;
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete'); // this references the CSS rule .delete{width:fit-content;} to size the delete button
+    li.append(deleteButton);
+    list.append(li);
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent); // note this new function that is needed to remove the chapter from the array and localStorage.
+        input.focus();
+    });
+};
+
+function setChapterList() {
+    localStorage.setItem('BOMchapterList', JSON.stringify(chaptersArray));
+};
+
+function getChapertList() {
+    return JSON.parse(localStorage.getItem('BOMchapterList'))
+};
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
